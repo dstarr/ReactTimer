ff36af689679472e70d858a4fe1cd2ff7eb0324d
David Starr <dxstarr@godaddy.com>
2017-10-16 08:06:33 -0700

    removed ununsed property in App

--- components/App.js
+++ components/App.js
@@ -5,7 +5,7 @@ import AppContainer from './AppContainer';
 const node1 = document.getElementById('app');
 
 if (node1) {
-    ReactDOM.render( <AppContainer who="React.js"/>, node1);
+    ReactDOM.render( <AppContainer/>, node1);
 }
 
 
